7246b1792b95ff3147a765e247eac2bceef6599c
unknown <ajborza1@gmail.com>
2021-03-06 16:06:12 -0500

    final

--- mod6_solution/app.js
+++ mod6_solution/app.js
@@ -1,3 +1,10 @@
+
+// Anthony Borza
+// Assignment 6
+// Due Date: 3/9/2021
+// app.js: contains business logic for handling number of lunch
+// items a user can enter in to a textbox. 
+
 (function (){
 
 'use strict';
@@ -11,15 +18,19 @@ LunchCheckController.$inject = ['$scope'];
 
 	function LunchCheckController($scope){
 
+		// scope vairables that are used as the binding part
+		// for html and javascript controller
 		$scope.items = '';
 		$scope.message = '';
 
+		// function is called in html page
 		$scope.results = function(){
-		
+
 		var splitString = $scope.items.split(",");
 	
 			$scope.count = 0;
 
+			// condition to handle empty entries
 			if($scope.items == 0){
 				$scope.message = "Please enter data first";
 				$scope.count = 0;	
@@ -28,8 +39,8 @@ LunchCheckController.$inject = ['$scope'];
 					'background-border': 'red'
 					 };	
 				count = 0;						// count will be 0 when there is nothing entered
-
 			}
+			// condition to handle entries that are less than or equal to 3
 			else if(splitString.length <= 3){
 				$scope.message = "Please Enjoy!";
 				$scope.color = {
@@ -37,8 +48,8 @@ LunchCheckController.$inject = ['$scope'];
 					'background-border': 'red'
 					};
 				$scope.count = splitString.length;
-
 			}
+			// condition to handle entries greater than 3
 			else if(splitString.length > 3){
 				$scope.message = "Thats two Much!";
 				$scope.color = {

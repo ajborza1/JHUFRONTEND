53db4e5223fa5866e6aa8d563121b7ffb418d080
unknown <ajborza1@gmail.com>
2021-03-06 15:48:58 -0500

    updates

--- mod6_solution/app.js
+++ mod6_solution/app.js
@@ -3,7 +3,8 @@
 'use strict';
 
 // declare angular module 
-angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+angular.module('LunchCheck', [])
+	   .controller('LunchCheckController', LunchCheckController);
 
 // declare an inject property
 LunchCheckController.$inject = ['$scope'];
@@ -13,18 +14,21 @@ LunchCheckController.$inject = ['$scope'];
 		$scope.items = '';
 		$scope.message = '';
 
-
 		$scope.results = function(){
 		
-			var splitString = $scope.items.split(",");
+		var splitString = $scope.items.split(",");
+	
+			$scope.count = 0;
 
 			if($scope.items == 0){
 				$scope.message = "Please enter data first";
+				$scope.count = 0;	
 				$scope.color = {
 					'color': 'red',
 					'background-border': 'red'
-					 };
-				
+					 };	
+				count = 0;						// count will be 0 when there is nothing entered
+
 			}
 			else if(splitString.length <= 3){
 				$scope.message = "Please Enjoy!";
@@ -32,6 +36,8 @@ LunchCheckController.$inject = ['$scope'];
 					'color': 'green',
 					'background-border': 'red'
 					};
+				$scope.count = splitString.length;
+
 			}
 			else if(splitString.length > 3){
 				$scope.message = "Thats two Much!";
@@ -39,8 +45,8 @@ LunchCheckController.$inject = ['$scope'];
 					'color': 'green',
 					'background-border': 'green'
 					};
-			}
-		
+				$scope.count = splitString.length;
+			}	
 		}
 	};
 

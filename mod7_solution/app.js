ef510c4d4993b6341149074b16730ae256b25049
unknown <ajborza1@gmail.com>
2021-03-14 15:43:03 -0400

    start

---
+++ mod7_solution/app.js
@@ -0,0 +1,119 @@
+
+
+// Anthony Borza
+// Assignment 7
+// Due Date: 3/23/2021
+// app.js: 
+
+(function (){
+
+'use strict';
+
+// declare angular module 
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController',ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListService', ShoppingListService);
+
+// declare two contollers using the singleton 
+// approach to share data
+
+ToBuyController.$inject = ['ShoppingListService'];
+function ToBuyController(ShoppingListService){
+	
+	// reference toBuyItem
+	var toBuyItem = this;
+
+	toBuyItem.itemName = "";
+	toBuyItem.itemQuantity = "";
+	
+	// show items for purchases
+	toBuyItem.buyItems = ShoppingListService.getBuyItems();
+
+  	toBuyItem.addBuyItem = function () {
+    	ShoppingListService.addBuyItem(toBuyItem.itemName, toBuyItem.itemQuantity);
+  	};
+
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListService'];
+function AlreadyBoughtController(ShoppingListService){
+
+	var showBought = this;
+
+	// show bought items
+	showBought.boughtItems = ShoppingListService.getBoughtItems();
+
+	// puts removed element from buyItem in showBought
+
+
+	// showBought.moveBuyToBought = function(itemIndex, buyItems, boughtItems){
+	// 	ShoppingListService.moveBuyToBought(itemIndex, buyItems, boughtItems);
+	// };
+}
+
+
+function ShoppingListService(){
+
+	var service = this;
+
+
+	// array to hold list of shopping items
+	var boughtItems = [];
+
+	// add 5 items to the by section
+	var buyItems =
+	[
+		{
+			name: "cookies",
+		 	quantity: "10"
+		}, 
+		{
+			name: "Coke", 
+			quantity: "3"
+		},
+		{
+			name: "Chips",
+			quantity: "4"
+		},
+		{
+			name: "Candy",
+			quantity: "2"
+		},
+		{
+			name: "Apples", 
+			quantity: "6"
+		}
+	];
+
+
+	service.addBuyItem = function(itemName, quantity){
+	  var boughtItems =
+	   {
+	      name: itemName,
+	      quantity: quantity
+	   };
+	    buyItems.push(boughtItems);
+	}
+
+
+	// moves elements from one buyItems array to boughtItems array
+	service.moveBuyToBought = function (itemIndex, buyItems, boughtItems){
+		boughtItems.push(buyItems[itemIndex]);
+		buyItems.splice(itemIndex, 1);
+	};
+
+
+	// show items that are ready to buy
+	service.getBuyItems = function(){
+		return buyItems;
+	};
+
+	// show items that have been bought
+	service.getBoughtItems = function(){
+		return boughtItems;
+	};
+}
+
+
+}());

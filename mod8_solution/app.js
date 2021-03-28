e2141871bb2644a7118a0ba3dc2431a8963c4a13
unknown <ajborza1@gmail.com>
2021-03-28 16:13:24 -0400

    updates

--- mod8_solution/app.js
+++ mod8_solution/app.js
@@ -1,3 +1,9 @@
+// Anthony Borza
+// Assignment 8
+// Due Date: 4/06/2021
+// app.js: This class handles the business logic for
+// reaching out to a a site using a get request, 
+// and pulling the results down via by search term
 
 (function (){
 
@@ -10,8 +16,10 @@ angular.module('NarrowItDownApp', [])
 .directive('foundItems', FoundItemsDirective);									// foundItems directive
 
 
-
 // Declare and create foundItems directive. 
+// takes the found array
+// provides an on-remove attribute
+// parent controller is invoked as a list
 function FoundItemsDirective() {
 
   var ddo = {
@@ -28,87 +36,82 @@ function FoundItemsDirective() {
   return ddo;
 }
 
-
-// NarrowItDownController should be injected with MenuSearchService
-// when found should be stored in found property that is 
-// attached to controller instance
-// create a promise for searchTerm
-// call getMatchedMenuItems()
-// function to remove items
-// function to display error message
+// NarrowItDownController injected with MenuSearchService
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService){
 
 	var list = this;
 
+	// empty array for found items
 	list.found = [];
 
+	// holds the serach term
 	list.searchTerm = "";
 
+	// calls the getMatchMenuItems and stores
+	// the results in a propery called found that
+	// is attached it controller isntance
 	list.getMatchedMenuItems = function(searchTerm){
 		if(list.searchTerm == ""){
 			list.found = [];
-			return ;
+			return;
 		}
-
+		// create a promise for searchTerm
 		var promise = MenuSearchService.getMatchedMenuItems(list.searchTerm);
 
 		promise.then(function(response){
-
 			list.found = response;
 			console.log(list.found);
 		})
 		.catch(function(error){
-
 			console.log(error);
 		})
 	};
 
+	// function to remove items
 	list.removeItem = function(itemIndex){
 		list.found.splice(itemIndex, 1);
 	};
-
+	
+	// function to display error message
 	list.errorMessage = function(){
 		if(list.found.length === 0){
-			console.log(list.found);
+			console.log("Nothing found!")
 			return true;
 		}
 		return false;
 	}
 }
 
-// reaches out to the server (using the $htpp service)
-// to retrive the list of all themenu items.
-// loop through the menu items to determine
-// which ones whoes description matches the searchTerm
+// MenuSearchService function
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath){
 
 	var service = this;
 
+	// declate an empty array 
 	var foundItems = [];
 	
+	// Reaches out to the server (using the $http service) to 
+	// retrieve the list of all the menu items.
 	service.getMatchedMenuItems = function(searchTerm){
 		return $http({
-			
 			method: 'GET',
 			url: (ApiBasePath + "/menu_items.json"),
-   	
 		}).then(function(result){
 
 			var menu = result.data.menu_items;
 
-			for(var i = 0; i < menu.length; i++){
-				
+			// loops through all the menu items to pick out the ones 
+			// whose description matches the searchTerm
+			// when a search item is found, it pushes it to the foundItems array
+			for(var i = 0; i < menu.length; i++){				
 				var item = menu[i].short_name.toLowerCase().indexOf(searchTerm.toLowerCase());
-
 				if(item !== -1){
 					foundItems.push(menu[i]);
 				}
 			}
-			console.log(foundItems);
 			return foundItems;
-
 		});
 	}
 }
